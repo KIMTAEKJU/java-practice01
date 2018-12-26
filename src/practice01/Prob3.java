38c13b1c6ba9e5a3d1b5c0181e0d1d22b0160fd5
bit <bit@DESKTOP-0G705V6>
2018-12-26 19:08:19 +0900

    문제해결했습니다 (틀렸다면 수정하겠습니다)

--- src/practice01/Prob3.java
+++ src/practice01/Prob3.java
@@ -1,7 +1,28 @@
 package practice01;
 
-public class Prob3 {
-	
-	public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Prob3 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner( System.in );
+
+		int userInput = 0; // 유저인풋값
+		int sum = 0; // 합계
+		
+		System.out.print("숫자를 입력하세요 : ");
+		userInput = scanner.nextInt();
+		
+		if (userInput % 2 == 0) // 짝수
+			for (int i = 2; i <= userInput; i+=2)
+				sum += i;
+		
+		else // 홀수
+			for (int i = 1; i <= userInput; i+=2)
+				sum += i;
+		
+		System.out.println("결과 값 : " + sum);
+		scanner.close();
 	}
 }
