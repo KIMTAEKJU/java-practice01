944a31ab7c1941c44f7fee35eee37508aa0dd8c9
bit <bit@DESKTOP-0G705V6>
2018-12-26 19:48:56 +0900

    문제 해결했습니다 (틀렸다면 수정하겠습니다)

--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -1,9 +1,28 @@
 package practice01;
 
+import java.util.Scanner;
 
-public class Prob4 {
-
-	public static void main(String[] args) {
+public class Prob4 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner( System.in );
+		
+		String userInput = "";
+		int lenCount = 0; // 문자표시개수 카운트
+		
+		System.out.print("문자열을 입력하세요. : ");
+		userInput = scanner.nextLine();
+		
+		while (userInput.length() != lenCount)
+		{
+			for (int i = 0; i <= lenCount; i++)
+				System.out.print(userInput.charAt(i)); //하나씩 가져와서 출력
+			System.out.println();
+			
+			lenCount++; // 카운트증가
+		}
+		
 	}
 
 }
--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -1,7 +1,35 @@
 package practice01;
 
-public class Prob5 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Prob5 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner( System.in );
+		String charValue = "";
+		int count = 0; // 3, 6, 9 세는 카운트
+		
+		for (int i = 1; i <= 999; i++)
+		{
+			charValue = String.valueOf(i);
+			
+			for (int j = 0; j < charValue.length(); j++)
+				// 3 6 9가 있다면 카운트 증가
+				if (charValue.charAt(j) == '3' || charValue.charAt(j) == '6' || charValue.charAt(j) == '9')
+					count++;
+			
+			
+			if (count != 0) // 3 6 9가 하나라도 있을때
+			{
+				System.out.print(i + " ");
+			
+				for (int k = 0; k < count; k++) //카운트수만큼 짝 출력
+					System.out.print("짝");
+				System.out.println();
+			}
+			
+			count = 0;
+		}
 	}
 }
