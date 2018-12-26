1189be44053f0552eb89fba3e1c8f3fb80240965
bit <bit@DESKTOP-0G705V6>
2018-12-26 18:58:11 +0900

    문제 해결입니다 (틀렸다면 수정하겠습니다)

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -1,6 +1,18 @@
 package practice01;
 
-public class Prob2 {
-	public static void main(String[] args) {
+public class Prob2 
+{
+	public static void main(String[] args) 
+	{
+		int max = 10;
+		
+		for (int i = 1; i <= 9; i++)
+		{
+			for (int j = i; j <= max; j++)
+				System.out.print(j + " ");
+			System.out.println();
+			
+			max++; // 최댓값 1씩 증가
+		}
 	}
 }
